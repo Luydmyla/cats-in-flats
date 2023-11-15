52443f9b2eebd9fea7a3a785611d23113d89c668
Liudmyla <87442018+Luydmyla@users.noreply.github.com>
2023-11-16 01:06:52 +0200

    update imgUrl  in cats

--- src/components/Cats/cats.js
+++ src/components/Cats/cats.js
@@ -2,11 +2,7 @@ export default [
   {
     id: "5f689a2c09b16000e9a2f47b",
     title: "Барбарис",
-<<<<<<< HEAD
     imgUrl: "./img/picture-3.jpg",
-=======
-    imgUrl: "/img/picture-3.jpg",
->>>>>>> ad7f99f71bcdacf7c7da7d24ce66d802c12619cd
     descr: "  Хижий для ворогів, ласкавий та м’якесенький до своїх.",
     rating: 3,
     price: 1667,
@@ -22,11 +18,7 @@ export default [
   {
     id: "5f689a2c09b16000e9a2f47d",
     title: "Мишка",
-<<<<<<< HEAD
     imgUrl: "./img/picture-1.jpg",
-=======
-    imgUrl: "/cats-in-flats/public/img/picture-1.jpg",
->>>>>>> ad7f99f71bcdacf7c7da7d24ce66d802c12619cd
     descr: " Киця з роду котів-талісманів. Несе удачу та затишок в дім.",
     rating: 3,
     price: 2740,
@@ -42,11 +34,7 @@ export default [
   {
     id: "5f689a2c09b16000e9a2f48a",
     title: "Капітан",
-<<<<<<< HEAD
     imgUrl: "./img/picture-2.jpg",
-=======
-    imgUrl: "/public/img/picture-2.jpg",
->>>>>>> ad7f99f71bcdacf7c7da7d24ce66d802c12619cd
     descr: " Всюди головний. Підкорить тебе, але ти будеш щасливий.",
     rating: 1,
     price: 2153,
