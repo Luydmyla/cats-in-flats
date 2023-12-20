ffd020a879e6a52391ecd144a6581fb2d6f1134c
Liudmyla <87442018+Luydmyla@users.noreply.github.com>
2023-12-20 19:16:35 +0200

    added mobile style to heroItem

--- src/components/Hero/HeroItem.vue
+++ src/components/Hero/HeroItem.vue
@@ -46,20 +46,13 @@
 <style lang="scss" scoped>
 @media (min-width: 280px) {
 .hero-item{
-    /* position: relative; */
     display: flex;  
-     /* display: block;   */
-         height: 60vh;
-        /* justify-content: center; */
+        height: 60vh;
         align-items: end;
         flex-shrink: 0;
         background-color: #7EA6A7;
-        /* background: url(../../../public/img/hero2.png)  no-repeat ; */
-              background: url(/public/img/hero2.png)  no-repeat ;
-          /* background: url(/img/hero2.png)  no-repeat ; */
-        /* background-position: top; */
+        background: url(/public/img/hero2.png)  no-repeat ;
         background-size: cover;
-        /* background-size: contain; */
         margin-bottom: 10px;
         border-radius: 16px;
         padding: 4px;
@@ -67,64 +60,51 @@
 
  }
 .greeting{ 
-    /* position:absolute;
-    top: 100%;
-    left: 0; */
     display: inline-flex;
-        padding-bottom: 4px;
-        flex-direction: column;
-        align-items: center;
-        width: 100%;
-        margin-bottom: 0;
-        /* height: 216px; */
-        border-radius: 10px;
-        /* background-color: hsl(50 33% 25% / 0.75);  */
-       background-color:  #fdcd7e transparent;
-    /* font-family: Montserrat;
-    font-size: 14px;
-    font-style: normal;
-    font-weight: 700;
-    line-height: 32px;
-   
-    letter-spacing: 1.25px; */
+    padding-bottom: 4px;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    margin-bottom: 0;
+    padding: 8px;
+    border-radius: 10px;
+    background-color: rgb(234,236,234, 0.9);
+    /* background-color:  #fdcd7e transparent; */
         }
         .greeting-title{
-            display: flex;
-                flex-direction: column;
-                justify-content: center;
-                /* color: #33665b; */
-                color: var(--WHITE, #FFF);
-                font-family: Montserrat;
-                font-size: 24px;
-                font-style: normal;
-                font-weight: 700;
-                line-height: 24px;
-                letter-spacing: 1.25px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;   
+        color: #33665b; 
+        font-family: Montserrat;
+        font-size: 24px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: 24px;
+        letter-spacing: 1.25px;
         }
  .greeting-text{
     display: flex;
     text-align: center;
-            color: #33665b;    
-                 color: var(--WHITE, #FFF);
-            font-family: Montserrat;
-            font-size: 14px;
-            font-style: normal;
-            font-weight: 400;
-            line-height: 14px;
-            letter-spacing: 0.5px;
-            /* width: 100%; */
+    color: #33665b;    
+    /* color: var(--WHITE, #FFF); */
+    font-family: Montserrat;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 14px;
+    letter-spacing: 0.5px;
         }
         .greeting-textfooter{
-            color: #33665b;  
-                 color: var(--WHITE, #FFF);  
+            /* color: #33665b;  */
+            color: #F07F2E; 
+            /* color: var(--WHITE, #FFF);   */
             font-family: Montserrat;
             font-size: 14px;
             font-style: normal;
             font-weight: 400;
             line-height: 14px;
             letter-spacing: 0.5px;
-                /* height: 20%; */
-            /* width: 100%; */
             margin-top: 4px;
             padding: 4px;
             background-color: rgb(243,246,249, 0.75);
@@ -137,37 +117,27 @@
 @media (min-width: 1024px) {
 .hero-item{
     display: flex;
-      height: 60vh;
-        justify-content: center;
-        align-items: center;
-        flex-shrink: 0;
-        background-color: #7EA6A7;
-        background: url(../../../public/img/hero2.png)  no-repeat ;
-        background-position: top;
-          /* background: url(../../../public//img/8-29CQ5tmymwbEv2W.png) center no-repeat ; */
-        /* background-repeat: no-repeat; */
-        background-size: cover;
-        /* background-size: contain; */
-        margin-bottom: 60px;
-        border-radius: 16px;
+    height: 60vh;
+    justify-content: center;
+    align-items: center;
+    flex-shrink: 0;
+    background-color: #7EA6A7;
+    background: url(../../../public/img/hero2.png)  no-repeat ;
+    background-position: top;
+    background-size: cover;
+    margin-bottom: 60px;
+    border-radius: 16px;
  }
 .greeting{
-    /* position: absolute; */
-    /* top: 136px; */
-   /* bottom: 28px; */
-    /* left: 28px; */
     display: inline-flex;
-        padding: 24px;
-        flex-direction: column;
-        /* align-items: flex-start; */
-        gap: 8px;
-        width: 432px;
-        margin-bottom: 0;
-        /* height: 216px; */
-        border-radius: 16px;
-        /* background-color: rgb(253,205,126, 0.75); */
-             background-color: rgb(234,236,234, 0.9);
-       /* background-color:  #fdcd7e; */
+    padding: 24px;
+    flex-direction: column;
+    gap: 8px;
+    width: 432px;
+    margin-bottom: 0;
+    padding: 8px;
+    border-radius: 16px;
+    background-color: rgb(234,236,234, 0.9);
     font-family: Montserrat;
     font-size: 32px;
     font-style: normal;
@@ -177,42 +147,33 @@
         }
  .greeting-text{
     color: #33665b;
-        /* color: var(--WHITE, #FFF); */    
-            font-family: Montserrat;
-            font-size: 24px;
-            font-style: normal;
-            font-weight: 400;
-            line-height: 28px;
-            letter-spacing: 0.5px;
-            /* width: 384px; */
+    font-family: Montserrat;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 28px;
+    letter-spacing: 0.5px;
         }
  .greeting-textfooter{
-            color: #33665b;    
-            font-family: Montserrat;
-             font-size: 24px;
-            font-style: normal;
-            font-weight: 700;
-            line-height: 24px;
-            letter-spacing: 0.5px;
-            margin-top: 20px;
-            color: #F07F2E;
-
+    /* color: #33665b;     */
+    font-family: Montserrat;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 24px;
+    letter-spacing: 0.5px;
+    margin-top: 20px;
+    color: #F07F2E;
         }
 
         .greeting-title{
-            /* display: flex; */
-                /* width: 313px; */
-                /* height: 40px; */
-                /* flex-direction: column; */
-                /* justify-content: center; */
-                color: #33665b;
-                /* color: var(--WHITE, #FFF); */
-                font-family: Montserrat;
-                font-size: 36px;
-                font-style: normal;
-                font-weight: 700;
-                line-height: 24px;
-                letter-spacing: 1.25px;
+        color: #33665b;
+        font-family: Montserrat;
+        font-size: 36px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: 24px;
+        letter-spacing: 1.25px;
         }
 }
 </style>
