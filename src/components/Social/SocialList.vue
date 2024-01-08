10cb86dcf72c4a6d0f87fb3bfaff154486e304d3
Liudmyla <87442018+Luydmyla@users.noreply.github.com>
2024-01-08 02:51:28 +0200

    change url social icons

--- src/components/Social/SocialList.vue
+++ src/components/Social/SocialList.vue
@@ -2,10 +2,10 @@
           <!-- <section section class="social" > -->
                   <!-- <p class="social-text">Соцмережі</p> -->
                   <ul class="social-links">
-<SocialItem href="/public/img/icons.svg#icon-instagram"/>
-    <SocialItem href="/public/img/icons.svg#icon-facebook"/>
-    <SocialItem href="/public/img/icons.svg#icon-youtube"/>
-    <SocialItem href="/public/img/icons.svg#icon-tiktok"/>
+<SocialItem href="/img/icons.svg#icon-instagram"/>
+    <SocialItem href="/img/icons.svg#icon-facebook"/>
+    <SocialItem href="/img/icons.svg#icon-youtube"/>
+    <SocialItem href="/img/icons.svg#icon-tiktok"/>
                     <!-- <li class="social-item">
                       <a href="">
                         <svg>
